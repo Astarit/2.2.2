0c28060d4283d28a7eab97bb1e842704048601d1
SidorovVladimir <samoxod@list.ru>
2024-04-03 17:22:40 +0300

    2.2.2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,18 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String printCars(ModelMap model, @RequestParam(value = "count", defaultValue = "5") int count) {
+		List<Car> cars = new ArrayList<>();
+		cars.add(new Car("Gaz", 1231, "red"));
+		cars.add(new Car("Vaz", 27545, "blue"));
+		cars.add(new Car("Maz", 2345, "green"));
+		cars.add(new Car("Liaz", 4455, "brown"));
+		cars.add(new Car("Kraz", 1001, "grey"));
+		model.addAttribute("cars", cars.subList(0, count));
+		return "cars";
 	}
 	
 }
